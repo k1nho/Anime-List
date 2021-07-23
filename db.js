1a768f90a0bdbe72c096e7aab440d9338f8bf698
kinho <kinhong99@gmail.com>
2021-07-22 20:16:49 -0400

    change ssl format to object

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
-  sslmode: "require",
+  ssl: { rejectUnauthorized: false },
 };
 
 const pool = new Pool(
